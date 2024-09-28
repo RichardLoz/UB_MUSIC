0962880aa01a8416000b435a47754ead85d986cf
RichardLoz <lozanorichard32@gmail.com>
2024-09-28 11:40:34 -0300

    Solucionando errores de Login

--- UB_Music/src/App.jsx
+++ UB_Music/src/App.jsx
@@ -1,48 +1,15 @@
-<<<<<<< HEAD
-//import './App.css'
+// import './App.css'
 // import Home from './screens/home'
 import Login from './components/Login.jsx'
-=======
-import { useState } from 'react'
-// Modulos de Firebase
-import appFirebase from './credentials'
-import {getAuth, onAuthStateChanged} from 'firebase/auth'
-
-const auth = getAuth(appFirebase)
-
-import Home from './components/Home/Home'
-import Login from './components/Login/Login'
-
-
-import './App.css'
->>>>>>> 5ada5bd818e25783aa84f95989bd1ce2141ddeed
 
 function App() {
 
-  const [user, setUser] = useState(null)
-
-  onAuthStateChanged(auth, (userFirebase)=>{
-    if (userFirebase){
-      setUser(userFirebase)
-    }
-    else
-    {   
-      setUser(null)
-    }
-  })
 
   return (
-<<<<<<< HEAD
     <>
     <Login/>
      {/* <Home/> */}
     </>
-=======
-    <div>
-      {/* {user ? <Home userEmail = {user.email}/> : <Login/>} */}
-      <Login/>
-    </div>
->>>>>>> 5ada5bd818e25783aa84f95989bd1ce2141ddeed
   )
 }
 
--- UB_Music/tailwind.config.js
+++ UB_Music/tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< HEAD
   content: [],
-=======
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
->>>>>>> 5ada5bd818e25783aa84f95989bd1ce2141ddeed
   theme: {
     extend: {},
   },
